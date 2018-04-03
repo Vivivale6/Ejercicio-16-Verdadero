ab7c5a42ea45d57ef60e8fd03291187a16812bb0
Vivivale6 <37335436+Vivivale6@users.noreply.github.com>
2018-04-03 06:51:08 -0500

    Add files via upload

---
+++ ejercicio.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int num;
+
+void numeros(int n);
+
+void numeros(int n){
+  int i;
+  for (i in n){
+    num=+i;
+  }
+
+
+}
+
+int main(){
+
+  void numeros(100);
+  cout << num << endl;
+  
+
+  
+  return 0;
+
+}
